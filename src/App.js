a456b228a494164e8c3364aff6a28e1c1e4d7b85
Vel MAr <velmar.annabell@gmail.com>
2023-03-19 18:16:52 +0200

    add latest recipes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
 import Home from './components/home/Home';
@@ -9,17 +9,32 @@ import Main from './components/main/Main';
 import Login from './components/forms/Login';
 import Register from './components/forms/Register';
 import CreatePage from './components/forms/CreatePage';
+import { useEffect, useState } from 'react';
 
-
+const baseUrl='http://localhost:3030/data/recipes'
+// const urlLatestRecipes=`http://localhost:3030/data/recipes?sortBy=_createdOn%20desc&distinct=category`
 
 function App() {
+  const [recipes,setRecipes] = useState([]);
+
+  useEffect(()=>{
+    fetch(baseUrl)
+    .then(res=>res.json())
+    .then(result=>{
+      // console.log(result);
+      setRecipes(result)
+     
+    })
+    console.log(recipes);
+  },[])
+  console.log(recipes);
   return (
     <div id="container">
       <Header />
 
       <Main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home recipes={recipes}/>} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register/>} /> 
           <Route path="/create" element={<CreatePage />} />
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,14 +1,16 @@
 
 import './Home.css'
 import OtherRecipes from './OtherRecipes';
-const Home = () => {
+const Home = ({
+    recipes,
+}) => {
+    const latestThreeRecipes = recipes.slice(0,3)
     return (<section id="dashboard-page" className="dashboard">
-        <h1>Dashboard</h1>
+        <h1>Latest Added Recipes</h1>
         {/* Display ul: with list-items for All books (If any) */}
         <ul className="other-recipes-list">
-            <OtherRecipes />
-            <OtherRecipes />
-            <OtherRecipes />
+            {latestThreeRecipes.map(x=> <OtherRecipes recipe={x}/>)}
+            
         </ul>
         {/* Display paragraph: If there are no books in the database */}
         <p className="no-recipes">No Recipes in database!</p>
