31c5362b7749aad1f27e88bead8f6e557095be64
Vel MAr <velmar.annabell@gmail.com>
2023-12-18 15:19:37 +0200

    fix password error message

--- src/components/validations/registerValidation.js
+++ src/components/validations/registerValidation.js
@@ -4,7 +4,7 @@ const registerSchema = yup.object().shape({
     email:yup.string().email("Please enter a valid email address!").required("Please enter email address!"),
     firstName: yup.string().required("Please enter First Name"),
     lastName: yup.string().required("Please enter Last Name"),
-    password: yup.string().min(4).max(10).required("Please enter password!"),
+    password: yup.string().min(4, 'Password must be at least 4 characters!').max(10,'Password should not exceed 10 characters!').required("Please enter password!"),
     confirmPass:  yup.string().oneOf([yup.ref("password"),null], "Passwords don't match!").required("Please repeat password!")
   });
 
